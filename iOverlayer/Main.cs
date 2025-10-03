84ca938065c23e2de49e3452357c7cc5b26b16b1
StarR1ver <2326837228@qq,com>
2025-10-04 00:09:11 +0800

    添加项目

---
+++ iOverlayer/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+using HarmonyLib;
+using UnityModManagerNet;
+namespace iOverlayer
+{
+    public static class Main
+    {
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        internal static TextBehavior gui;
+        public static Harmony Harmony;
+        public static void Load(UnityModManager.ModEntry modEntry)
+        {
+            gui = new GameObject().AddComponent<TextBehavior>();
+            gui.setText("uwu");
+            gui.setPosition(0.5f,0.5f);
+            UnityEngine.Object.DontDestroyOnLoad(gui);
+            gui.TextObject.SetActive(true);
+            modEntry.Logger.Log("Hello World");
+        }
+    }
+}
---
+++ iOverlayer/TextShower.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+namespace iOverlayer
+{
+    internal class TextBehavior : MonoBehaviour
+    {
+        public GameObject TextObject;
+        public TextMeshProUGUI text;
+        public Shadow shadowText;
+        public RectTransform rectTransform;
+
+         public void setSize(int size)
+        {
+            text.fontSize = size;
+            text.rectTransform.sizeDelta = new Vector2(text.preferredWidth, text.preferredHeight);
+        }
+        public void setText(string text) {
+            this.text.text= text;
+        }
+
+        public void setPosition(float x, float y)
+        {
+            Vector2 pos = new Vector2(x, y);
+            rectTransform.anchorMin = pos;
+            rectTransform.anchorMax = pos;
+            rectTransform.pivot = pos;
+        }
+
+        void Awake()
+        {
+            // 创建Canvas
+            Canvas mainCanvas = gameObject.AddComponent<Canvas>();
+            mainCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            mainCanvas.sortingOrder = 10001;
+            CanvasScaler scaler = gameObject.AddComponent<CanvasScaler>();
+            scaler.referenceResolution = new Vector2(1920, 1080);
+            
+            // 创建Text
+            TextObject = new GameObject();
+            TextObject.transform.SetParent(transform);
+            TextObject.AddComponent<Canvas>();
+            rectTransform = TextObject.GetComponent<RectTransform>();
+            
+            GameObject textObject = new GameObject();
+            textObject.transform.SetParent(TextObject.transform);
+            
+            text = textObject.AddComponent<TextMeshProUGUI>();
+            text.font = TMP_Settings.defaultFontAsset;
+            text.alignment = TextAlignmentOptions.Center;
+            text.fontSize = 32;
+            text.color = Color.cyan;
+            text.overflowMode = TextOverflowModes.Overflow;
+            
+            shadowText = textObject.AddComponent<Shadow>();
+            shadowText.effectColor = new Color(0f, 0f, 0f, 0.5f);
+            shadowText.effectDistance = new Vector2(2f, -2f);
+
+            Vector2 pos = new Vector2(0, 0);
+            rectTransform.anchorMin = pos;
+            rectTransform.anchorMax = pos;
+            rectTransform.pivot = pos;
+            rectTransform.anchoredPosition = Vector2.zero;
+
+        }
+
+       
+    }
+}
