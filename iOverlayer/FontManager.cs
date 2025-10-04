1eccd590bbcded1804d60e9dd35b5758fd2f36e3
shadows2023 <18858122027@163.com>
2025-10-04 19:17:13 +0800

    添加变量

--- iOverlayer/FontManager.cs
+++ iOverlayer/FontManager.cs
@@ -1,9 +1,19 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 
 namespace iOverlayer
 {
     public class FontManager
     {
-        private static TMP_FontAsset DefaultTMP;
+        static TMP_FontAsset DefaultTMP;
+        static Font DefaultFont;
+        static bool initalized;
+        static FontData defaultFont;
+        static Dictionary<string, FontData> Fonts;
+        public static bool Initialize() => initalized;
+        public static string[] OSFonts { get; private set; }
+        public static string[] OSFontsPath { get; private set; }
+
     }
 }
