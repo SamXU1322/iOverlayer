452cd18219cdb33a563d9f357423e64365efc726
shadows2023 <18858122027@163.com>
2025-10-26 17:10:02 +0800

    完成bpm修正

---
+++ iOverlayer/Tags/Patches/BPMPatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HarmonyLib;
+using iOverlayer.Core;
+
+namespace iOverlayer.Tags.Patch
+{
+    internal static class BPMPatch
+    {
+        private static BPM _bpm = new BPM();
+        [HarmonyPatch(typeof(scrPlanet), "MoveToNextFloor")]
+        internal class scrPlanet_MoveToNextFloor_Patch
+        {
+            public static void Postfix(scrPlanet __instance,scrFloor floor)
+            {
+                _bpm.bpm = __instance.conductor.bpm;
+                if(!__instance.controller.gameworld) return;
+                if (floor.nextfloor == null) return;
+                bool isTwirl = scrController.instance.isCW;
+                double val = scrMisc.GetAngleMoved(floor.entryangle,floor.exitangle,isTwirl)/3.1415927410125732*180;
+                double angle =Math.Round(val);
+                if(angle==0) angle=360;
+                double speed = 0;
+                speed = __instance.controller.speed;
+                ModManager.TextBehaviors[0].text.text=$"CurBPM:{Math.Round(_bpm.bpm*speed*180/angle,3)}";
+            }
+        }
+    }
+    
+}
