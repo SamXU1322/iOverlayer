1c5597062587c075b577f6b63d869ea028b666a4
shadows2023 <18858122027@163.com>
2025-10-26 22:23:55 +0800

    完成游戏时间显示

--- iOverlayer/Tags/Patches/BPMPatch.cs
+++ iOverlayer/Tags/Patches/BPMPatch.cs
@@ -4,15 +4,15 @@ using iOverlayer.Core;
 
 namespace iOverlayer.Tags.Patch
 {
-    internal static class BPMPatch
+    public static class BPMPatch
     {
-        private static BPM _bpm = new BPM();
+        public static BPM Bpm = new BPM();
         [HarmonyPatch(typeof(scrPlanet), "MoveToNextFloor")]
-        internal class scrPlanet_MoveToNextFloor_Patch
+        public class scrPlanet_MoveToNextFloor_Patch
         {
             public static void Postfix(scrPlanet __instance,scrFloor floor)
             {
-                _bpm.bpm = __instance.conductor.bpm;
+                Bpm.bpm = __instance.conductor.bpm;
                 if(!__instance.controller.gameworld) return;
                 if (floor.nextfloor == null) return;
                 bool isTwirl = scrController.instance.isCW;
@@ -21,7 +21,6 @@ namespace iOverlayer.Tags.Patch
                 if(angle==0) angle=360;
                 double speed = 0;
                 speed = __instance.controller.speed;
-                ModManager.TextBehaviors[0].text.text=$"CurBPM:{Math.Round(_bpm.bpm*speed*180/angle,3)}";
             }
         }
     }
---
+++ iOverlayer/Tags/Song.cs
@@ -0,0 +1,8 @@
+﻿namespace iOverlayer.Tags
+{
+    public class Song
+    {
+        public int CurMinute,CurSecond,CurMillisecond;
+        public int TotalMinute,TotalSecond,TotalMillisecond;
+    }
+}
