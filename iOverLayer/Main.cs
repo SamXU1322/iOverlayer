53e329508265eb04c662a3558bc82ca60a61eb70
shadows2023 <18858122027@163.com>
2026-02-01 14:32:30 +0800

    Initial commit

---
+++ iOverLayer/Main.cs
@@ -0,0 +1,10 @@
+﻿using UnityModManagerNet;
+namespace iOverLayer{
+    public static class Main
+    {
+        public static void Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.Logger.Log("iOverLayer loaded.");
+        }
+    }
+}
