0d459b2cf4527f2ce48c3f14acf40ffeb729da97
shadows2023 <18858122027@163.com>
2026-05-17 00:15:12 +0800

    an access blank was created a new window was created and the test passed

---
+++ iOverLayer/Info.cs
@@ -0,0 +1,10 @@
+namespace iOverlayer
+{
+    public static class Info
+    {
+        public const string Name = "iOverlayer";
+        public const string Version = "1.0.0";
+        public const string Author = "Sam1na & StarRiver";
+        public const string DownloadUrl = "https://github.com/SamXU1322/iOverlayer";
+    }
+}
--- iOverLayer/Main.cs
+++ iOverLayer/Main.cs
@@ -1,1 +1,29 @@
-﻿
+using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(iOverlayer.Core), iOverlayer.Info.Name, iOverlayer.Info.Version, iOverlayer.Info.Author, iOverlayer.Info.DownloadUrl)]
+[assembly: MelonGame("7th Beat Games", "A Dance of Fire and Ice")]
+
+namespace iOverlayer
+{
+    public class Core : MelonMod
+    {
+        private Rect windowRect = new Rect(20, 20, 240, 80);
+
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("iOverlayer loaded");
+        }
+
+        public override void OnGUI()
+        {
+            windowRect = GUI.Window(0, windowRect, DrawWindow, "iOverlayer");
+        }
+
+        private void DrawWindow(int windowID)
+        {
+            GUI.Label(new Rect(10, 20, 220, 40), "hello iOverlayer");
+            GUI.DragWindow(new Rect(0, 0, 10000, 20));
+        }
+    }
+}
